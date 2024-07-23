e6019c5ff389f97876a18c4bccd42af1eb9ca27d
yongen.loong <yongen.loong@aelf.io>
2024-07-23 16:47:18 +0800

    add docker

--- appsettings.ts
+++ appsettings.ts
@@ -1,8 +1,8 @@
 export const APP_SETTINGS: IAppSettings = {
-  "chainGPT": {
+  chainGPT: {
     // For vercel: key, secret, etc. get from .env
-    "apiKey": process.env.CHAINGPT_API_KEY as string,
-  }
+    apiKey: process.env["CHAINGPT_API_KEY"] as string,
+  },
 };
 
 export interface IAppSettings {
